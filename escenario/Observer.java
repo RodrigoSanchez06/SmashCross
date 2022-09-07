9a58eebe6311560ff7e735792d895fe3b8a6b53f
Rodrigo Sánchez <91684997+RodrigoSanchez06@users.noreply.github.com>
2022-09-06 22:17:11 -0500

    Delete escenario directory

--- escenario/Observer.java
+++
@@ -1,6 +0,0 @@
-package escenario;
-
-public interface Observer {
-    public void update();
-}
-
--- escenario/Subject.java
+++
@@ -1,7 +0,0 @@
-package escenario;
-
-public interface Subject {
-    public void register(Viewer viewer);
-    public void remove(Viewer viewer);
-    public void communicate();
-}
